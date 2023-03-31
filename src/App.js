1ffd0e637884fac28410d5c9a1459b156c3ac9b9
Gary H <3rdeye.ghj@gmail.com>
2023-03-31 12:09:45 -0700

    Trying to understand API requests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import Header from './Components/Header';
 import Table from './Components/Table';
-
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then(response => {
+        setData(response.data.results);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      <Table />
+      <Table data={data} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Header () {
   return (
     <div>
-      <h1 className="text-center">Star Wars Canon Search</h1>
+      <h1 className="text-center text-warning">Star Wars Canon Search</h1>
     </div>
   )
 }
