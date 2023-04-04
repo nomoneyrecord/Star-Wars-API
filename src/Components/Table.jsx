c5b5d784f2111d773614558ba75c608424d0e4e8
Gary H <3rdeye.ghj@gmail.com>
2023-04-04 14:33:02 -0700

    Data is working but slowly

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -4,50 +4,80 @@ import axios from "axios";
 
 function Table() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://swapi.dev/api/people");
+    const results = response.data.results;
+
+    const dataWithDetails = await Promise.all(
+      results.map(async (person) => {
+        const homeworldResponse = await axios.get(person.homeworld);
+        const homeworld = homeworldResponse.data.name;
+
+        let species = "Unknown";
+        if (person.species.length > 0) {
+          const speciesResponse = await axios.get(person.species[0]);
+          species = speciesResponse.data.name;
+        } else {
+          species = "Human";
+        }
+
+        return {
+          ...person,
+          homeworld,
+          species,
+        };
+      })
+    );
+
+    // Filter the data based on the user's input
+    const filteredData = dataWithDetails.filter((person) => {
+      const nameMatches = person.name.toLowerCase().includes(searchTerm.toLowerCase());
+      const homeworldMatches = person.homeworld.toLowerCase().includes(searchTerm.toLowerCase());
+      const speciesMatches = person.species.toLowerCase().includes(searchTerm.toLowerCase());
+      return nameMatches || homeworldMatches || speciesMatches;
+    });
+
+    setData(filteredData);
+  };
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await axios.get("https://swapi.dev/api/people");
-      const results = response.data.results;
-
-  
-      const dataWithDetails = await Promise.all(
-        results.map(async (person) => {
-          const homeworldResponse = await axios.get(person.homeworld);
-          const homeworld = homeworldResponse.data.name;
-
-          let species = "Unknown";
-          if (person.species.length > 0) {
-            const speciesResponse = await axios.get(person.species[0]);
-            species = speciesResponse.data.name;
-          } else {
-            species = "Human";
-          }
-
-          return {
-            ...person,
-            homeworld,
-            species,
-          };
-        })
-      );
-
-      setData(dataWithDetails);
+    if (searchButtonClicked) {
+      fetchData().catch((error) => {
+        console.error(error);
+      });
+    } else {
+      setData([]);
     }
+  }, [searchTerm, searchButtonClicked]);
 
-    fetchData().catch((error) => {
-      console.error(error);
-    });
-  }, []);
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchButtonClick = () => {
+    setSearchButtonClicked(true);
+    fetchData();
+  };
 
   return (
     <div className="container">
       <div className="row mb-3">
         <div className="col">
-          <input type="text" className="form-control" placeholder="Enter Text" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Text"
+            value={searchTerm}
+            onChange={handleSearchInputChange}
+          />
         </div>
         <div className="col-auto">
-          <button className="btn btn-primary">Search</button>
+          <button className="btn btn-primary" onClick={handleSearchButtonClick}>
+            Search
+          </button>
         </div>
       </div>
       <div className="table-responsive">
@@ -81,3 +111,4 @@ function Table() {
 }
 
 export default Table;
+
